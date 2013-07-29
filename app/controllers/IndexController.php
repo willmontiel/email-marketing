0b5844cc704bedc5a2d05d7dee147863cd4f093d
Will Montiel <william.montiel@sigmamovil.com>
2013-07-29 13:36:26 -0500

    no message

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,13 +4,8 @@ class IndexController extends \Phalcon\Mvc\Controller
 {
     public function indexAction()
     {
-<<<<<<< HEAD
         
         $name=$this->session->get("user-name", $u); 
-=======
-        $nom="prueba1";
-        $this->session->set("user-name", $nom);
-		
->>>>>>> 9753e5c9141cfbcbe576a145c4d8a2384de6b64e
+
     }
 }
