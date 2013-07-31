d33289fa7e2683d02a5e32958c69392c202256b7
Will Montiel <william.montiel@sigmamovil.com>
2013-07-31 11:00:33 -0500

    template y Customfield

---
+++ app/models/Customfield.php
@@ -0,0 +1,32 @@
+<?php
+use Phalcon\Mvc\Model\Validator\PresenceOf;
+//use Phalcon\Mvc\Model\Validator\Uniqueness;
+
+class Customfield extends \Phalcon\Mvc\Model
+{
+	public $idBase;
+	public $idCustomField;
+	
+	public function initialize()
+    {
+        $this->belongsTo("idCustomField", "Dbase", "idDbase", array(
+            "foreignKey" => true,
+        ));
+		$this->useDynamicUpdate(true);
+    }
+
+	public function validation()
+    {
+		$this->validate(new PresenceOf(
+            array(
+                "field"   => "name",
+				"message" => "Debes asignar un nombre al campo personalizado"
+            )
+        ));
+		
+		if ($this->validationHasFailed() == true) {
+			return false;
+        }
+		
+	}
+}
