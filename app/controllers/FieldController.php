38157a0f34fd5f204676f6a5b24c614b0ec6df18
Will Montiel <william.montiel@sigmamovil.com>
2013-07-31 12:18:06 -0500

    validacion de campos en modelo Customfield

--- app/controllers/FieldController.php
+++ app/controllers/FieldController.php
@@ -17,13 +17,7 @@ class FieldController extends ControllerBase
 			$this->db->begin();
 			$field->idDbase = $idbase;
 			$field->idAccount = Dbase::findFirstByIdDbase($idbase)->idAccount;
-			
-			if(empty($field->values) || empty($field->required))
-			{
-				$field->values="Vacío";
-				$field->required=2;
-			}	
-			
+	
 				if (!$form->isValid() OR !$field->save()) {
 				
 				$this->db->rollback();
--- app/models/Customfield.php
+++ app/models/Customfield.php
@@ -1,6 +1,6 @@
 <?php
 use Phalcon\Mvc\Model\Validator\PresenceOf;
-//use Phalcon\Mvc\Model\Validator\Uniqueness;
+use Phalcon\Mvc\Model\Validator\Uniqueness;
 
 class Customfield extends \Phalcon\Mvc\Model
 {
@@ -11,20 +11,41 @@ class Customfield extends \Phalcon\Mvc\Model
     {
     
 		$this->useDynamicUpdate(true);
+		
     }
-
+	public function beforeValidationOnCreate()
+    {
+        if (!isset($this->values) || $this->values == NULL) {
+			$this->values = "";
+        }
+		
+		if (!isset($this->required) || $this->required == NULL) {
+			$this->required = 2;
+        }
+    }
 	public function validation()
     {
+		
+		
 		$this->validate(new PresenceOf(
-            array(
-                "field"   => "name",
-				"message" => "Debes asignar un nombre al campo personalizado"
+				array(
+					"field"   => "name",
+					"message" => "Debes asignar un nombre al campo personalizado"
             )
         ));
 		
+		$this->validate(new Uniqueness(
+				array(
+					"field" => "name",
+					"message" => "No debes tener campos con el mismo nombre"
+				)
+		));
+	
 		if ($this->validationHasFailed() == true) {
 			return false;
         }
+		
+		
 		
 	}
 }
