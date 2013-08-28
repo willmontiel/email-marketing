a657b55fc12aba34c94b5abcb74c78727e0d7eec
Ivan Dario Barona <ivan.barona@sigmamovil.com>
2013-08-28 11:23:57 -0500

    Continuo con las pruebas de Contadores

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -11,11 +11,58 @@ class TestController extends ControllerBase
 
 	public function testcountersAction()
 	{
-		$email = "hola@holasrc.com";
-		$idAccount = 3;
+		$email = "holacomotah@hola.com";
 		$idDbase = 1156;
-		$idList = 1;
+		$idList = 5;
+		$log = $this->logger;
 		
+		$db = Dbase::findFirstByIdDbase($idDbase);
+		$list = Contactlist::findFirstByIdList($idList);
+		
+		$array[0] = $this->fullArray($db, $list);
+		$array[0]['Desc'] = "Estado Inicial";
+
+		//-----------------New Contact-----------------//
+		
+		$contact = $this->createContact($email, $idDbase, $idList);
+		
+		$idContact = $contact->idContact;
+		
+		$db = Dbase::findFirstByIdDbase($idDbase);
+		$list = Contactlist::findFirstByIdList($idList);
+		
+		$array[1] = $this->fullArray($db, $list);
+		$array[1]['Desc'] = "Nuevo Contacto";
+
+		//-----------------Update Contact-----------------//
+	
+		$this->updateContact($email, $idDbase, $idContact);
+		
+		$db = Dbase::findFirstByIdDbase($idDbase);
+		$list = Contactlist::findFirstByIdList($idList);
+		
+		$array[2] = $this->fullArray($db, $list);
+		$array[2]['Desc'] = "Contacto Actualizado";
+		
+		
+		$this->view->setVar("results", $array);	
+	}
+	
+	protected function updateContact($email, $idDbase, $idContact)
+	{
+		$wrapper = new ContactWrapper();
+		
+		$wrapper->setAccount($this->user->account);
+		$wrapper->setIdDbase($idDbase);
+		$wrapper->setIPAdress($_SERVER["REMOTE_ADDR"]);
+		
+		$contact = $this->createContactObj($email, "", 1, "", "");
+		
+		//$wrapper->updateContactFromJsonData($idContact, $contact);
+	}	
+
+	protected function createContact($email, $idDbase, $idList)
+	{
 		$wrapper = new ContactWrapper();
 		
 		$wrapper->setAccount($this->user->account);
@@ -23,42 +70,50 @@ class TestController extends ControllerBase
 		$wrapper->setIdList($idList);
 		$wrapper->setIPAdress($_SERVER["REMOTE_ADDR"]);
 		
-		$contact = $this->createContactObj($email, "", 1);
+		$contact = $this->createContactObj($email, 1, 1, "", "");
 		
-		$contactC = $wrapper->createNewContactFromJsonData($contact);
-		
-		$db = Dbase::findFirstByIdDbase($idDbase);
-		
-		$this->result[0] = array(
-			"Ctotal" => $db->Ctotal,
-			"Cactive" => $db->Cactive,
-			"Cunsubscribed" => $db->Cunsubscribed,
-			"Cbounced" => $db->Cbounced,
-			"Cspam" => $db->Cspam
-		);
-		
-		$this->view->setVar("resutlados", $this->result[0]);	
+		//$wrapper->createNewContactFromJsonData($contact);
 	}
-	
+
 	protected function createContactObj($email, $isactive = "", $issubscribed = "", $isbounced = "", $isspam = "")
 	{
-		$obj = array();
-		$obj['email'] = $email;
-		$obj['name'] = "";
-		$obj['last_name'] = "";
-		$obj['status'] = "";
-		$obj['activated_on'] = "";
-		$obj['bounced_on'] = "";
-		$obj['subscribed_on'] = "";
-		$obj['unsubscribed_on'] = "";
-		$obj['spam_on'] = "";
-		$obj['ip_active'] = "";
-		$obj['ip_subscribed'] = "";
-		$obj['updated_on'] = "";
-		$obj['created_on'] = "";
-		$obj['is_bounced'] = $isbounced;
-		$obj['is_subscribed'] = $issubscribed;
-		$obj['is_spam'] = $isspam;
-		$obj['is_active'] = $isactive;
+		$obj = new stdClass();
+		
+		$obj->email = $email;
+		$obj->name = "";
+		$obj->last_name = "";
+		$obj->status = "";
+		$obj->activated_on = "";
+		$obj->bounced_on = "";
+		$obj->subscribed_on = "";
+		$obj->unsubscribed_on = "";
+		$obj->spam_on = "";
+		$obj->ip_active = "";
+		$obj->ip_subscribed = "";
+		$obj->updated_on = "";
+		$obj->created_on = "";
+		$obj->is_bounced = $isbounced;
+		$obj->is_subscribed = $issubscribed;
+		$obj->is_spam = $isspam;
+		$obj->is_active = $isactive;
+		
+		return $obj;
+	}
+	
+	protected function fullArray($db, $list)
+	{
+		$array = array(
+			"CtotalDB" => $db->Ctotal,
+			"CactiveDB" => $db->Cactive,
+			"CunsubscribedDB" => $db->Cunsubscribed,
+			"CbouncedDB" => $db->Cbounced,
+			"CspamDB" => $db->Cspam,
+			"CtotalList" => $list->Ctotal,
+			"CactiveList" => $list->Cactive,
+			"CunsubscribedList" => $list->Cunsubscribed,
+			"CbouncedList" => $list->Cbounced,
+			"CspamList" => $list->Cspam
+		);
+		return $array;
 	}
 }
--- app/views/test/testcounters.volt
+++ app/views/test/testcounters.volt
@@ -1,15 +1,79 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        {{ get_title() }}
+        {{ stylesheet_link('bootstrap/css/bootstrap.css') }}
+        {{ stylesheet_link('css/style.css') }}
+        {{ stylesheet_link ('css/flat-ui.css') }}
+        {{ stylesheet_link ('css/bootstrap-modal.css') }}
+        {{ stylesheet_link ('css/prstyles.css') }}
+		{{ stylesheet_link ('css/style.css') }}
+		{{ stylesheet_link ('css/emarketingstyle.css') }}
+    </head>
+    <body>
+
 <div class="row-fluid">
- 	 <div class="span12" >
- 		<table class='table table-striped'>
- 		 {%for resutlado in resutlados%}
+ 	 <div class="span6" >
+ 		<table class="table">
+			<thead>
+				<tr>
+					<th> </th>
+					<th colspan="5">Base de Datos</th>
+					<th colspan="5">Listas</th>
+				</tr>
+				<tr>
+					<th>
+						Descripcion
+					</th>
+					<th>
+						Total Contactos
+					</th>
+					<th>
+						Activos
+					</th>
+					<th>
+						Des-Suscritos
+					</th>
+					<th>
+						Rebotados
+					</th>
+					<th>
+						Spam
+					</th>
+					<th>
+						Total Contactos
+					</th>
+					<th>
+						Activos
+					</th>
+					<th>
+						Des-Suscritos
+					</th>
+					<th>
+						Rebotados
+					</th>
+					<th>
+						Spam
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+ 		 {%for result in results%}
  			<tr>
- 				<td>{{resutlado.Ctotal}}</td>
-				<td>{{resutlado.Cactive}}</a></td>
-				<td>{{resutlado.Cunsubscribed}}</td>
- 				<td>{{resutlado.Cbounced}}</td>
- 				<td>{{resutlado.Cspam}}</td>
+				<td>{{result["Desc"]}}</td>
+ 				<td>{{result["CtotalDB"]}}</td>
+				<td>{{result["CactiveDB"]}}</a></td>
+				<td>{{result["CunsubscribedDB"]}}</td>
+				<td>{{result["CbouncedDB"]}}</td>
+ 				<td>{{result["CspamDB"]}}</td>
+				<td>{{result["CtotalList"]}}</td>
+				<td>{{result["CactiveList"]}}</a></td>
+				<td>{{result["CunsubscribedList"]}}</td>
+				<td>{{result["CbouncedList"]}}</td>
+ 				<td>{{result["CspamList"]}}</td>
  			</tr>
  		 {%endfor%}
+			</tbody>
  	    </table>
  	 </div>
 </div>
