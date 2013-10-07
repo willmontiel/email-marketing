ab91fb86471446caa78b9342a3fc9dfc36386b5c
Ivan Dario Barona <ivan.barona@sigmamovil.com>
2013-10-07 12:33:31 -0500

    Cache Security

--- app/plugins/Security.php
+++ app/plugins/Security.php
@@ -23,7 +23,7 @@ class Security extends Plugin
 		/*
 		 * Buscar ACL en cache
 		 */
-		$acl = null; //$this->cache->get('acl-cache');
+		$acl = $this->cache->get('acl-cache');
 		
 		if (!$acl) {
 			// No existe, crear objeto ACL
@@ -82,7 +82,7 @@ class Security extends Plugin
 	
 	protected function getControllerMap()
 	{
-		$map = null; //$this->cache->get('controllermap-cache');
+		$map = $this->cache->get('controllermap-cache');
 		if (!$map) {
 			$map = array(
 				//Tests
