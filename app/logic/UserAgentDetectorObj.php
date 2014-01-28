78f71ca4ed5c6fb66dc7268c8137eaf0d4a1f58a
Will Montiel <william.montiel@sigmamovil.com>
2014-01-28 11:11:31 -0500

    no message

--- app/logic/UserAgentDetectorObj.php
+++ app/logic/UserAgentDetectorObj.php
@@ -15,13 +15,13 @@ class UserAgentDetectorObj
 	public function getOperativeSystem()
 	{
 		if (preg_match('/linux/i', $this->info)) {
-			$so = 'Windows';
+			$so = 'Linux';
 		}
 		else if (preg_match('/macintosh|mac os x/i', $this->info)) {
 			$so = 'MacOs';
 		}
 		else if (preg_match('/windows|win32/i', $this->info)) {
-			$so = 'Linux';
+			$so = 'Windows';
 		}
 		else if (strpos('Android', $this->info)) {
 			$so = 'Android';
@@ -41,17 +41,14 @@ class UserAgentDetectorObj
 		elseif(preg_match('/Firefox/i',$this->info)){
 			$browser = 'Mozilla Firefox';
 		}
-		elseif(preg_match('/Opera/i',$this->info)){
-			$browser = 'Opera';
+		elseif(preg_match('/Chrome/i',$this->info) && (!preg_match('/Opera/i',$this->info)) || !preg_match('/OPR/i',$this->info)){
+			$browser = 'Google Chrome';
 		}
-		elseif(preg_match('/OPR/i',$this->info)){
+		elseif(preg_match('/Opera/i',$this->info) || preg_match('/OPR/i',$this->info)){
 			$browser = 'Opera';
 		}
 		elseif(preg_match('/Safari/i',$this->info)){
 			$browser = 'Apple Safari';
-		}
-		elseif(preg_match('/Chrome/i',$this->info)){
-			$browser = 'Google Chrome';
 		}
 		elseif(preg_match('/Netscape/i',$this->info)){
 			$browser = 'Netscape';
