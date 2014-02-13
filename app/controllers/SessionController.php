011d719e38a6cffa4fda319849dfce9b03463495
Will Montiel <william.montiel@sigmamovil.com>
2014-02-13 14:42:56 -0500

    se quitó el checkToken del login

--- app/controllers/SessionController.php
+++ app/controllers/SessionController.php
@@ -19,7 +19,7 @@ class SessionController extends \Phalcon\Mvc\Controller
     public function loginAction()
     {
 		if ($this->request->isPost()) {
-			if ($this->security->checkToken()) {
+//			if ($this->security->checkToken()) {
 				$login = $this->request->getPost("username");
 				$password = $this->request->getPost("pass");
 
@@ -36,7 +36,7 @@ class SessionController extends \Phalcon\Mvc\Controller
 					return $this->response->redirect("");
 
 				}
-			}
+//			}
         }
         
 		$this->flashSession->error('Usuario o contraseña incorrecta!');
