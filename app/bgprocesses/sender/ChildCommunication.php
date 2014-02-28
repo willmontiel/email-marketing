2af104f133b7fbd3a1e275781a04611bab42adca
Will Montiel <william.montiel@sigmamovil.com>
2014-02-28 11:22:10 -0500

    Correo de prueba sin el header MailClass

--- app/bgprocesses/sender/ChildCommunication.php
+++ app/bgprocesses/sender/ChildCommunication.php
@@ -143,7 +143,7 @@ class ChildCommunication extends BaseWrapper
 					else {
 						$mta = $this->account->virtualMta;
 					}
-					$headers->addTextHeader('X-GreenArrow-MailClass', 'SIGMA_NEWEMKTG_DEVEL');
+//					$headers->addTextHeader('X-GreenArrow-MailClass', 'SIGMA_NEWEMKTG_DEVEL');
 					$headers->addTextHeader('X-GreenArrow-MtaID', $mta);
 					$headers->addTextHeader('X-GreenArrow-InstanceID', '0em' . $mail->idMail);
 					$headers->addTextHeader('X-GreenArrow-Click-Tracking-ID', 'em' . $mail->idMail . 'x' . $contact['contact']['idContact']);
