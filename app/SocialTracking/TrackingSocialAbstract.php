13925c64a84e778a04351e68876586405f3b934b
Will Montiel <william.montiel@sigmamovil.com>
2014-03-04 10:10:38 -0500

    no message

--- app/SocialTracking/TrackingSocialAbstract.php
+++ app/SocialTracking/TrackingSocialAbstract.php
@@ -35,12 +35,12 @@ abstract class TrackingSocialAbstract
 	
 	public function save()
 	{
-		Phalcon\DI::getDefault()->get('logger')->log('Saving');
+		\Phalcon\DI::getDefault()->get('logger')->log('Saving');
 		if(!$this->mxc->save()) {
 			foreach ($this->mxc->getMessages() as $msg) {
-				Phalcon\DI::getDefault()->get('logger')->log('Error: ' . $msg);
+				\Phalcon\DI::getDefault()->get('logger')->log('Error: ' . $msg);
 			}
-			throw new Exception('Exception: Error while saving tracking open social in Mxc');
+			throw new \Exception('Exception: Error while saving tracking open social in Mxc');
 		}
 	}
 }
