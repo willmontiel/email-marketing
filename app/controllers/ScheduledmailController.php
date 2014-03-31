60f3e8aea2718f9ab352c053e6d97a805d11e9e7
Ivan Dario Barona <ivan.barona@sigmamovil.com>
2014-03-31 18:03:16 -0500

    no message

--- app/controllers/ScheduledmailController.php
+++ app/controllers/ScheduledmailController.php
@@ -18,13 +18,18 @@ class ScheduledmailController extends ControllerBase
 
 	public function indexAction()
 	{
-		$idAccount = $this->user->account->idAccount;
+		if($this->user->userrole == 'ROLE_SUDO') {
+			$account = "";
+		}
+		else {
+			$account = "idAccount = {$this->user->account->idAccount} AND deleted = 0";
+		}
 		
 		$currentPage = $this->request->getQuery('page', null, 1); // GET
 
 		$paginator = new \Phalcon\Paginator\Adapter\Model(
 			array(
-				"data" => Mail::find("idAccount = $idAccount AND status != 'Draft' AND deleted = 0"),
+				"data" => Mail::find("{$account}status != 'Draft' ORDER BY scheduleDate"),
 				"limit"=> PaginationDecorator::DEFAULT_LIMIT,
 				"page" => $currentPage
 			)
