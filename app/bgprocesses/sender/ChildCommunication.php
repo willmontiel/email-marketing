be4f00847944998644207d066787f94254122a4b
Will Montiel <william.montiel@sigmamovil.com>
2014-04-01 14:57:24 -0500

    no message

--- app/bgprocesses/sender/ChildCommunication.php
+++ app/bgprocesses/sender/ChildCommunication.php
@@ -1,5 +1,5 @@
 <?php
-require_once "/../../library/swiftmailer/lib/swift_required.php";
+require_once "../../app/library/swiftmailer/lib/swift_required.php";
 class ChildCommunication extends BaseWrapper
 {
 	protected $childprocess;
