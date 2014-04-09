858cd6d1314fb8b076f337fb40ff171469c909fb
Ivan Dario Barona <ivan.barona@sigmamovil.com>
2014-04-09 15:56:21 -0500

    600 * 600

--- app/logic/SocialNetworkConnection.php
+++ app/logic/SocialNetworkConnection.php
@@ -5,8 +5,8 @@ class SocialNetworkConnection
 	public $twitter = null;
 	public $user;
 	
-	const IMG_SN_WIDTH = 470;
-	const IMG_SN_HEIGHT = 245;
+	const IMG_SN_WIDTH = 600;
+	const IMG_SN_HEIGHT = 600;
 	
 	function __construct($logger = null) {
 		$this->logger = $logger;
