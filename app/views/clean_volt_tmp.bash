f80d6b9ec9977b17a16d353bd9f743c536ced20a
Hector Lasso <hector.lasso@sigmamovil.com>
2014-04-15 10:32:10 -0500

    Script para eliminar archivos de VOLT temporales

---
+++ app/views/clean_volt_tmp.bash
@@ -0,0 +1,14 @@
+# !/bin/bash
+echo "Este script eliminara todos los archivos volt*.php... usar con precaucion"
+read -p "Estas seguro? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Ss]$ ]]
+then
+	echo "Estos son los archivos que se eliminan!!!"
+	find -regex ".*\.volt.*\.php"
+	echo "ELIMINANDO!!!"
+	find -regex ".*\.volt.*\.php" -delete
+	echo "Listo"
+else
+	echo "No se elimino nada!"
+fi
