589d0fc2a509db7a98a4b7cc74a6f0f0740eda34
Ivan Dario Barona <ivan.barona@sigmamovil.com>
2014-06-03 10:24:17 -0500

    sql_mode session

--- app/logic/ImportContactWrapper.php
+++ app/logic/ImportContactWrapper.php
@@ -435,14 +435,21 @@ class ImportContactWrapper
 		$this->saveProcess();
 		$rpath = realpath($tmpFilename);
 		$fields = implode(',', $this->fieldmapper->getFieldnames() );
+		$sql_db_mode = "SET session sql_mode=''";
 		$importfile = "LOAD DATA INFILE '{$rpath}' INTO TABLE {$this->tablename} FIELDS TERMINATED BY '{$delimiter}' OPTIONALLY ENCLOSED BY '\"'"
 					. "({$fields})";
+		$sql_db_mode_strict = "SET session sql_mode='strict_all_tables'";
 		
 		$this->log->log("SQL: {$importfile}");
 		// Ejecutar sentencia SQL
+		
+		$this->db->execute($sql_db_mode);
+		
 		$this->db->execute($importfile);
 		$this->timer->endTimer('load-rows');
-
+		
+		$this->db->execute($sql_db_mode_strict);
+		
 		$this->updateProcessStatus('Mapeando contactos');
 		$this->saveProcess();
 
