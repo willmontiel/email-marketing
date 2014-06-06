d55194fbdb9bf5607400e750b4e0f2f29dba69eb
Will Montiel <william.montiel@sigmamovil.com>
2014-06-06 17:49:35 -0500

    no message

--- app/general/Misc/AccountingObject.php
+++ app/general/Misc/AccountingObject.php
@@ -82,18 +82,18 @@ class AccountingObject
 	
 	protected function processData($values)
 	{
-		foreach ($this->accounts as $account) {
-			foreach ($values as $value) {
-				if ($account->idAccount !== $value['idAccount']) {
-					$values[] = array(
-						'idAccount' => $account->idAccount,
-						'account' => $account->companyName,
-						'lastMonth' => 0,
-						'currentMonth' => 0
-					);
-				}
-			}
-		}
+//		foreach ($values as $value) {
+//			foreach ($this->accounts as $account) {
+//				if ($account->idAccount !== $value['idAccount']) {
+//					$values[] = array(
+//						'idAccount' => $account->idAccount,
+//						'account' => $account->companyName,
+//						'lastMonth' => 0,
+//						'currentMonth' => 0
+//					);
+//				}
+//			}
+//		}
 		
 		return $values;
 	}
