122d540b203e34a7377ed66d630b14475fb7c0ef
Will Montiel <william.montiel@sigmamovil.com>
2014-07-08 16:16:38 -0500

    no message

--- public/js/target-selection/list-panel-content.js
+++ public/js/target-selection/list-panel-content.js
@@ -1,5 +1,8 @@
 function ListPanelContent() {
 	this.selectedValue = '';
+	this.dataSource = [];
+	this.selectData = [];
+	this.selectedItems = [];
 }
 
 ListPanelContent.prototype = new PanelContent;
@@ -11,25 +14,126 @@ ListPanelContent.prototype.initialize = function(panel) {
 		$(this).remove();
 	});
 	
+	this.content.find('.sgm-add-item').on('click', function (e) {
+		var value = null;
+		var text = null;
+		
+		for (var i = 0; i < self.selectData.length; i++) {
+			if (self.selectData[i].id === self.selectedValue) {
+				value = self.selectData[i].id;
+				text = self.selectData[i].text;
+				
+//				self.selectData.splice(i, 1);
+				break;
+			}
+			else if (self.selectData[i] !== undefined && self.selectData[i].children !== undefined && self.selectData[i].children !== null) {
+				for (var j = 0; j < self.selectData[i].children.length; j++) {
+					if (self.selectData[i].children[j].id === self.selectedValue) {
+						value = self.selectData[i].children[j].id;
+						text = self.selectData[i].children[j].text;
+						
+//						self.selectData[i].children.splice(j, 1);
+						
+						if (self.selectData[i].children.length === 0) {
+							self.selectData.splice(i, 1);
+						}
+						
+						var newSelect = self.selectData[i];
+						self.selectData = [];
+						self.selectData.push(newSelect);
+						break;
+					}
+				}
+			}
+		}
+		
+		if (value !== null && text !== null) {
+			var item = $('<div class="sgm-item-added sgm-remove-item" data-value=' + value + '>\n\
+							  <span class="glyphicon glyphicon-remove"></span> \n\
+						      ' + text + '\
+						  </div>'); 
+			
+			self.content.find('.sgm-box-content').append(item);
+			
+			self.selectedItems.push(item);
+			
+			self.content.find('.sgm-remove-item').on('click', function (e) {
+				self.removeItem(e, this);
+			});
+			
+			self.initializeSelect2(self.selectData);
+		}
+	});
+	
 	var url = self.getUrlForDataSource();
 	var dataSource = new DataSourceForSelect(url);
-	var source;
 
 	dataSource.findDataSource().then(function() { 
-		source = dataSource.getDataSource();
-		self.initializeSelect2(source);
+		var source = dataSource.getDataSource();
+		self.dataSource = source;
+		self.selectData = source;
+		
+		self.initializeSelect2(self.selectData);
 	});
 	
 	panel.find('.sgm-panel-content').append(this.content);
+};
+
+ListPanelContent.prototype.removeItem = function (e, item) {
+	e.preventDefault();
+	var value = $(item).attr('data-value');
+	
+//	console.log(value);
+//	console.log(this.dataSource);
+	
+	for (var i = 0; i < this.dataSource.length; i++) {
+		if (this.dataSource[i].id === value) {
+			this.selectData.push(this.dataSource[i]);
+			break;
+		}
+		else if (this.dataSource[i] !== undefined && this.dataSource[i].children !== undefined && this.dataSource[i].children !== null) {
+			for (var j = 0; j < this.dataSource[i].children.length; j++) {
+				if (this.dataSource[i].children[j].id === value) {
+					
+					console.log(this.selectData);
+					console.log(this.dataSource[i].children[j]);
+					
+					var x = {
+						id: this.dataSource[i].id, 
+						text: this.dataSource[i].text,
+						children: this.dataSource[i].children[j]
+					};
+					
+					console.log(x);
+					this.selectData.push(x);
+					
+					console.log(this.selectData);
+					break;
+				}
+			}
+		}
+	}
+	
+	item.remove();
+	
+	var it = this.selectedItems.indexOf(item);
+	
+	if (it >= 0) {
+		var tor = this.selectedItems.splice(it, 1);
+		tor.remove();
+	}
+	
+	this.initializeSelect2(this.selectData);
 };
 
 ListPanelContent.prototype.createContent = function () {
 	this.content = $('<div class="sgm-target-selector">\n\
 						<div class="sgm-selector-content">\n\
 							<input type="hidden" class="select2"/>\n\
+							<span class="sgm-add-item sgm-button-add glyphicon glyphicon-plus"></span>\n\
 						</div>\n\
+						<div class="sgm-box-content"></div>\n\
 						<div class="sgm-add-filter-content"></div>\n\
-						</div> \n\
 					 </div>');
 };
 
