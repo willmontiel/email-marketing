184f086b749a74f3e552840323846fea412ae47c
Will Montiel <william.montiel@sigmamovil.com>
2014-07-22 16:33:26 -0500

    no message

--- app/logic/TargetWrapper.php
+++ app/logic/TargetWrapper.php
@@ -39,19 +39,19 @@ class TargetWrapper extends BaseWrapper
 
 			switch ($this->data[0]['serialization']['criteria']) {
 				case 'dbases':
-					$this->SQLfilter->mail = " JOIN Contact AS c ON (c.idContact = mc.idContact) WHERE c.idDbase IN ({$ids}) AND m.status = 'Sent' GROUP BY 1,2";
+					$this->SQLfilter->mail = " JOIN Contact AS c ON (c.idContact = mc.idContact) WHERE c.idDbase IN ({$ids}) AND m.status = 'Sent' GROUP BY 1,2,3,4";
 					$this->SQLfilter->click = " JOIN Dbase AS d ON (d.idDbase = c.idDbase) WHERE d.idDbase IN ({$ids}) GROUP BY 1,2";
 					$this->SQLfilter->totalContacts = " WHERE idDbase IN ({$ids})";
 					break;
 
 				case 'contactlists':
-					$this->SQLfilter->mail = " JOIN Coxcl AS lc ON (lc.idContact = mc.idContact) WHERE lc.idContactlist IN ({$ids}) AND m.status = 'Sent' GROUP BY 1,2";
+					$this->SQLfilter->mail = " JOIN Coxcl AS lc ON (lc.idContact = mc.idContact) WHERE lc.idContactlist IN ({$ids}) AND m.status = 'Sent' GROUP BY 1,2,3,4";
 					$this->SQLfilter->click = " JOIN Coxcl AS cl ON (cl.idContact = c.idContact) WHERE cl.idContactlist IN ({$ids}) GROUP BY 1,2";
 					$this->SQLfilter->totalContacts = " JOIN Coxcl AS cl ON (cl.idContact = c.idContact) WHERE idContactlist IN ({$ids})";
 					break;
 
 				case 'segments':
-					$this->SQLfilter->mail = " JOIN Sxc AS sc ON (sc.idContact = mc.idContact) WHERE sc.idSegment IN ({$ids}) AND m.status = 'Sent' GROUP BY 1,2";
+					$this->SQLfilter->mail = " JOIN Sxc AS sc ON (sc.idContact = mc.idContact) WHERE sc.idSegment IN ({$ids}) AND m.status = 'Sent' GROUP BY 1,2,3,4";
 					$this->SQLfilter->click = " JOIN Sxc AS s ON (s.idContact = c.idContact) WHERE s.idSegment IN ({$ids}) GROUP BY 1,2 ";
 					$this->SQLfilter->totalContacts = " JOIN Sxc AS s ON (s.idContact = c.idContact) WHERE idSegment IN ({$ids})";
 					break;
@@ -63,7 +63,7 @@ class TargetWrapper extends BaseWrapper
 	{
 		$this->createSQLFilter();
 		
-		$this->sql = "SELECT m.idMail AS id, m.name AS name
+		$this->sql = "SELECT m.idMail AS id, m.name AS name, m.subject AS subject, m.startedon AS date
 					  FROM Mail AS m
 						  JOIN Mxc AS mc ON (mc.idMail = m.idMail) {$this->SQLfilter->mail}";
 		
@@ -94,6 +94,8 @@ class TargetWrapper extends BaseWrapper
 				$object = new stdClass();
 				$object->id = $r->id;
 				$object->text = $r->name;
+				$object->text = $r->subject;
+				$object->text = $r->date;
 //				$object->preview = $r->preview;
 				
 				$this->filter[] = $object;
