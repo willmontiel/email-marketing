209ac2c5821e07100a52c031b7b10589bf910d8b
Ivan Dario Barona <ivan.barona@sigmamovil.com>
2014-07-29 11:37:02 -0500

    no message

--- app/logic/NextMailingObj.php
+++ app/logic/NextMailingObj.php
@@ -38,8 +38,6 @@ class NextMailingObj {
 	public function getNextSendTime()
 	{
 		$today = strtotime( date('d-m-Y', time()) . ' ' . $this->time );
-		$this->logger->log('Antes de transformar ' . date('d-m-Y', time()) . ' ' . $this->time);
-		$this->logger->log('Despues de transformar ' . $today);
 		$today_txt = strtolower( date('l', time()) );
 		
 		if(in_array($today_txt, $this->days) && time() < $today ) {
@@ -59,16 +57,12 @@ class NextMailingObj {
 		if($this->days) {
 			foreach ($this->days as $day) {
 				$day_at_time = strtotime('next ' . $day . ' ' . $this->time );
-				$this->logger->log('Siguiente Fecha Antes: ' .'next ' . $day . ' ' . $this->time);
-				$this->logger->log('Siguiente Fecha Despues: ' . $day_at_time);
 				if($this->lastsend) {
-					$this->logger->log('Ultimo Envio ' . $this->lastsend );
 					$next_send = ( ( $next_send > $day_at_time || $next_send == 0 ) && ( $day_at_time > $this->lastsend ) ) ? $day_at_time : $next_send;
 				}
 				else {
 					$next_send = ( $next_send > $day_at_time || $next_send == 0 ) ? $day_at_time : $next_send;
 				}
-				$this->logger->log('Siguiente Envio ' , $next_send);
 			}
 		}
 		
