27b0f47144456f647c9e48bc7370a4b2107ec87d
Will Montiel <william.montiel@sigmamovil.com>
2014-07-30 16:37:41 -0500

    no message

--- public/js/target-selection/filter-panel-content.js
+++ public/js/target-selection/filter-panel-content.js
@@ -105,7 +105,7 @@ FilterPanelContent.prototype.createFilter = function(obj, container) {
 		if (self.selectedValue !== null) {
 			select.select2('val', self.selectedValue);
 			console.log(self.content.find('.sgm-add-neg'));
-			if (self.content.find('.sgm-add-neg').length < 0) {
+			if (self.content.find('.sgm-add-neg').length == 0) {
 				var active = (self.negation ? 'sgm-neg-active' :'');
 				var addNeg = $('<div class="sgm-add-neg ' + active + '">Not!</div>');
 				self.content.find('.sgm-content-negation-filter').append(addNeg);
@@ -129,7 +129,7 @@ FilterPanelContent.prototype.createFilter = function(obj, container) {
 				$(this).remove();
 			});
 			
-			if (self.content.find('.sgm-add-neg') < 0) {
+			if (self.content.find('.sgm-add-neg') == 0) {
 				console.log('1');
 			console.log(self.content.find('.sgm-add-neg'));
 			console.log('2');
