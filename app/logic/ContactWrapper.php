accd6cf76881c6ac595dfa76f596b267697157c8
Will Montiel <william.montiel@sigmamovil.com>
2014-08-12 16:52:49 -0500

    no message

--- app/logic/ContactWrapper.php
+++ app/logic/ContactWrapper.php
@@ -687,6 +687,7 @@ class ContactWrapper extends BaseWrapper
 		$object['email'] = $contact->email->email;
 		$object['name'] = $contact->name;
 		$object['birthDate'] = (!empty($contact->birthDate) ? $this->dateFormat->transformDateFormat($contact->birthDate, 'Y-m-d', 'd/m/Y') : '');
+//		$object['lastName'] = utf8_decode($contact->lastName);
 		$object['lastName'] = utf8_decode($contact->lastName);
 		$object['isActive'] = ($contact->status != 0);
 		$object['activatedOn'] = (($contact->status != 0)?date('d/m/Y H:i', $contact->status):'');
