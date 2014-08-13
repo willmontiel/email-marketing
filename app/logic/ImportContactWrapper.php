5e2c9b85b6b9266fd7b12f5a41c1d2a1c930f028
Will Montiel <william.montiel@sigmamovil.com>
2014-08-13 09:01:26 -0500

    no message

--- app/logic/ImportContactWrapper.php
+++ app/logic/ImportContactWrapper.php
@@ -552,7 +552,8 @@ class ImportContactWrapper
 	protected function copyCSVRecordsToPR($sourcefile, $tmpFilename, $delimiter, $maxrows, $hasHeader)
 	{		
 		$fp = fopen($sourcefile, 'r');
-		$nfp = fopen($tmpFilename, 'w');
+//		$nfp = fopen($tmpFilename, 'w');
+		$nfp = file_get_contents($tmpFilename);
 		
 		$rows = 0;
 
@@ -628,9 +629,8 @@ class ImportContactWrapper
 //				throw new \Exception('Codificacion invalida en texto');
 //			}
 //		}
-
-		file_put_contents($fh, "\xEF\xBB\xBF".  $line . "\n"); 
 //		fwrite($fh, $line . "\n"); 
+		file_put_contents($fh, "\xEF\xBB\xBF".  $line . "\n"); 
 	}
 	
 	protected function incrementProgress($adv)
