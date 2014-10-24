6de593338923a806a7289d4abaa76408a955eb67
Will Montiel <william.montiel@sigmamovil.com>
2014-10-24 11:58:10 -0500

    no message

--- app/general/AppObjects.php
+++ app/general/AppObjects.php
@@ -383,6 +383,7 @@ class AppObjects
 	{
 		$tmpdir = new \stdClass;
 		$tmpdir->dir = $this->config->general->tmpdir;
+		$tmpdir->exportdir = $this->config->general->tmpexportdir;
 		$this->di->set('tmppath', $tmpdir);
 	}
 	
