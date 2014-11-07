a9f25cdf1f4bc38f30c424bd7e17a15076bc44fb
Ivan Dario Barona <ivan.barona@sigmamovil.com>
2014-11-07 12:34:26 -0500

    Envio de correo, sin terminar

--- app/logic/MailApiWrapper.php
+++ app/logic/MailApiWrapper.php
@@ -121,4 +121,32 @@ class MailApiWrapper extends BaseWrapper
 					);
 		return $obj;
 	}
+	
+	public function send_mail_to_process($mail)
+	{
+		$schedule = Mailschedule::findFirstByIdMail($mail->idMail);
+
+		if($schedule) {
+			$mail->status = ($mail->status == 'Draft' || $mail->status == 'draft') ? 'Scheduled' : $mail->status;
+			
+			if(!$mail->save()) {
+				foreach ($mail->getMessages() as $msg) {
+					$this->logger->log($msg);
+				}
+				throw new Exception('Error saving mail in auto responder');
+			}
+
+			$schedule->confirmationStatus = 'Yes';
+
+			if(!$schedule->save()){
+				foreach ($schedule->getMessages() as $msg) {
+					$this->logger->log($msg);
+				}
+				throw new Exception('Error saving scheduling in auto responder');
+			}
+
+			$commObj = new Communication(SocketConstants::getMailRequestsEndPointPeer());
+			$commObj->sendSchedulingToParent($mail->idMail);	
+		}
+	}
 }
