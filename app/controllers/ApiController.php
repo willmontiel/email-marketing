d87eb95cbc04c8de945511f7e40fb02fbca1d6c8
Will Montiel <william.montiel@sigmamovil.com>
2014-12-10 08:40:11 -0500

    no message

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1826,6 +1826,8 @@ class ApiController extends ControllerBase
 			$wrapper->searchTotalContacts();
 			$sql = $wrapper->getSQL();
 			
+			$this->logger->log("SQL: {$sql}");
+			
 			if ($sql != false) {
 				$executer = new \EmailMarketing\General\Misc\SQLExecuter();
 				$executer->setSQL($sql);
