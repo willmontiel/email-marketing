19200ccfe3ce1ac85c5fdd70177852d61f4b7f18
Ivan Dario Barona <ivan.barona@sigmamovil.com>
2015-01-14 15:33:19 -0500

    Try - Catch en Child Process

--- app/bgprocesses/sender/ChildProcess.php
+++ app/bgprocesses/sender/ChildProcess.php
@@ -29,71 +29,76 @@ abstract class ChildProcess
 	
 	public function startProcess()
 	{
-		$this->pid = getmypid();
-		$context = new ZMQContext();
-		
-		$this->subscriber = new ZMQSocket($context, ZMQ::SOCKET_SUB);
-		$this->subscriber->connect($this->publishToChildren());
-		
-		$this->push = new ZMQSocket($context, ZMQ::SOCKET_PUSH);
-		$this->push->connect($this->pullFromChild());
-		
-		$filter = "$this->pid";
-		$this->subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, $filter);
-		
-		$poll = new ZMQPoll();
-		$poll->add($this->subscriber, ZMQ::POLL_IN);
-		$readable = $writable = array();
-		
-		while (true) {
-			$events = $poll->poll($readable, $writeable, 1000);
-			
-			if ($events && count($readable) > 0) {
-				
-				$request = $this->subscriber->recv(ZMQ::MODE_NOBLOCK);	
-				
-				if($request) {
-					
-					sscanf($request, "%d %s %s", $pid, $type, $data);
-					switch ($type) {
-						case 'Echo-Request':
-							$response = sprintf("%s %s Echo-Reply", 'Child-'.$this->pid, $data);
-							break;
-						case 'Echo-Tmp-Request':
-							$this->mode = 'TEMP';
-							
-							$response = sprintf("%s %s Echo-Tmp-Reply", 'Child-'.$this->pid, $data);
-							break;
-						case 'Processing-Task':
-							printf('Soy el PID ' . $pid . ' Y me Llego Esto: ' . $data . PHP_EOL);
+		try{
+			$this->pid = getmypid();
+			$context = new ZMQContext();
 
-							$this->executeProcess($data);
-							
-							Phalcon\DI::getDefault()->get('logger')->log(Phalcon\DI::getDefault()->get('timerObject'));
-							
-							printf('PID ' . $pid . ' Acabo' . PHP_EOL);
-							
-							$response = sprintf("%s %s Process-Available", 'Child-'.$this->pid, $this->pid);
-							break;
-						case 'Processing-Task':
-							$response = sprintf("%s %s %s", 'Child-'.$this->pid, 0, 'Work-Checked');
-							break;
-						case 'Echo-Kill':
-							printf($pid . ' Es hora de que muera' . PHP_EOL);
-							exit(0);
-							break;
+			$this->subscriber = new ZMQSocket($context, ZMQ::SOCKET_SUB);
+			$this->subscriber->connect($this->publishToChildren());
+
+			$this->push = new ZMQSocket($context, ZMQ::SOCKET_PUSH);
+			$this->push->connect($this->pullFromChild());
+
+			$filter = "$this->pid";
+			$this->subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, $filter);
+
+			$poll = new ZMQPoll();
+			$poll->add($this->subscriber, ZMQ::POLL_IN);
+			$readable = $writable = array();
+
+			while (true) {
+				$events = $poll->poll($readable, $writeable, 1000);
+
+				if ($events && count($readable) > 0) {
+
+					$request = $this->subscriber->recv(ZMQ::MODE_NOBLOCK);	
+
+					if($request) {
+
+						sscanf($request, "%d %s %s", $pid, $type, $data);
+						switch ($type) {
+							case 'Echo-Request':
+								$response = sprintf("%s %s Echo-Reply", 'Child-'.$this->pid, $data);
+								break;
+							case 'Echo-Tmp-Request':
+								$this->mode = 'TEMP';
+
+								$response = sprintf("%s %s Echo-Tmp-Reply", 'Child-'.$this->pid, $data);
+								break;
+							case 'Processing-Task':
+								Phalcon\DI::getDefault()->get('logger')->log('Soy el PID ' . $pid . ' Y me Llego Esto: ' . $data . PHP_EOL);
+
+								$this->executeProcess($data);
+
+								Phalcon\DI::getDefault()->get('logger')->log(Phalcon\DI::getDefault()->get('timerObject'));
+
+								Phalcon\DI::getDefault()->get('logger')->log('PID ' . $pid . ' Acabo' . PHP_EOL);
+
+								$response = sprintf("%s %s Process-Available", 'Child-'.$this->pid, $this->pid);
+								break;
+							case 'Processing-Task':
+								$response = sprintf("%s %s %s", 'Child-'.$this->pid, 0, 'Work-Checked');
+								break;
+							case 'Echo-Kill':
+								Phalcon\DI::getDefault()->get('logger')->log($pid . ' Es hora de que muera' . PHP_EOL);
+								exit(0);
+								break;
+						}
+						$this->push->send($response);
+						$this->lasttime = time();
 					}
-					$this->push->send($response);
-					$this->lasttime = time();
+				}
+				else {
+					if ((time() - $this->lasttime) > self::NUMBER_OF_SECONDS && $this->mode == 'TEMP') {
+						$response = sprintf("%s %s Kill-Process", 'Child-'.$this->pid, $this->pid);
+						$this->push->send($response);
+						exit(0);
+					}
 				}
 			}
-			else {
-				if ((time() - $this->lasttime) > self::NUMBER_OF_SECONDS && $this->mode == 'TEMP') {
-					$response = sprintf("%s %s Kill-Process", 'Child-'.$this->pid, $this->pid);
-					$this->push->send($response);
-					exit(0);
-				}
-			}
+			
+		} catch(Exception $ex) {
+			Phalcon\DI::getDefault()->get('logger')->log('Exception [ ' . $ex . ' ]');
 		}
 	}
 	
@@ -115,7 +120,7 @@ abstract class ChildProcess
 					 * ================================================================
 					 */
 					
-					printf($pid . ' Estoy trabajando pero debo morir' . PHP_EOL);
+					Phalcon\DI::getDefault()->get('logger')->log($pid . ' Estoy trabajando pero debo morir' . PHP_EOL);
 					exit(0);
 					break;
 			}
