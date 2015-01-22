c774e5aa8f6449ec13bb349a832232ed1fbf5218
Will Montiel <william.montiel@sigmamovil.com>
2015-01-22 10:43:45 -0500

    no message

--- app/logic/CodificShell.php
+++ app/logic/CodificShell.php
@@ -40,7 +40,8 @@ class CodificShell implements IElementProcessorRow
 		}
 		
 		foreach ($this->cmdline as $row) {
-			$cmdline[] = "pdftk {$explode}page_{$row['pagenum']}.pdf output {$encrypted}{$row['id']}.pdf user_pw {$row['password']} allow printing";
+			$page = trim($row['pagenum']);
+			$cmdline[] = "pdftk {$explode}page_{$page}.pdf output {$encrypted}{$row['id']}.pdf user_pw {$row['password']} allow printing";
 		}
 		
 		// grabar archivo de ejecucion
