9e1a989c54375e60e125b7a2460c36e1a5c5fd2c
unknown <william.montiel@liderdesarrollo.sigmamovil.local>
2015-05-14 15:54:20 -0500

    fifth test :(

--- app/logic/Reportingcreator.php
+++ app/logic/Reportingcreator.php
@@ -204,6 +204,7 @@ class Reportingcreator
             if (count($model->fields) > 0) {
                 foreach ($model->fields as $mfields1) {
                     $mfields1 = trim($mfields1);
+                    $this->logger->log($mfields1);	
                     if (!empty($mfields1)) {
                         $fieldsheader .= ", '{$mfields1}'";
                         if ($c1) {
