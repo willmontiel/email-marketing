ad93deb4a517c7e05de36d57bf7a26afc5eee766
unknown <dorian.lopez@desarrollo1.sigmamovil.local>
2015-09-17 10:44:07 -0500

    no message

--- app/bgprocesses/maintenance/statisticsSender.php
+++ app/bgprocesses/maintenance/statisticsSender.php
@@ -8,6 +8,8 @@ class StatisticsSender
 {
     public function init()
     {
+        echo "hola";
+        
         $mails = Mail::find(array(
             'conditions' => 'status = ?1 AND statisticsStatus = ?2',
             'bind' => array(1 => 'Sent',
