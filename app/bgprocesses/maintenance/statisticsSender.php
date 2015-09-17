95c6d4677152f6a162f71abdce9f9ab5415cccc2
unknown <dorian.lopez@desarrollo1.sigmamovil.local>
2015-09-17 15:20:38 -0500

    no message

--- app/bgprocesses/maintenance/statisticsSender.php
+++ app/bgprocesses/maintenance/statisticsSender.php
@@ -7,21 +7,28 @@ $sSender->init();
 class StatisticsSender
 {
     public function init()
-    {
-        echo "hola";
+    {        
+        $d = strtotime(date("d-m-Y h:m:s"));        
+        $towdays = strtotime("-2 days", $d);
+        
+        echo $d;
+        echo $towdays;
         
         $mails = Mail::find(array(
-            'conditions' => 'status = ?1 AND statisticsStatus = ?2',
+            'conditions' => 'status = ?1 AND statisticsStatus = ?2 AND finishedon < ?3 AND finishedon > ?4',
             'bind' => array(1 => 'Sent',
-                            2 => 0)
-        ));
+                            2 => 0,
+                            3 => $d,
+                            4 => $towdays)
+        ));        
         
-        $d = strtotime(date("Y-m-d"));        
-        $towdays = strtotime("+2 days", $d);
+        $dates = array();
         
         foreach ($mails as $mail) {
-            $d = ',' . $mail->finishedon;
-            echo $d;
+            $dates[] = $mail->finishedon;
+            echo $dates;
         }
+        
+        
     }    
 }
