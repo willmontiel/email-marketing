c65fbfe726855ab34d11dc38b04dff42aa6e5deb
unknown <dorian.lopez@desarrollo1.sigmamovil.local>
2015-09-17 15:32:53 -0500

    Ajuste

--- app/bgprocesses/maintenance/statisticsSender.php
+++ app/bgprocesses/maintenance/statisticsSender.php
@@ -8,18 +8,17 @@ class StatisticsSender
 {
     public function init()
     {        
-        $d = strtotime(date("d-m-Y h:m:s"));        
+        $d = strtotime(date("d-m-Y h:m:s"));
         $towdays = strtotime("-2 days", $d);
         
-        echo $d;
-        echo $towdays;
+        echo 'Dia de hoy: ' . $d;
+        echo 'Hace dos dias: ' . $towdays;
         
         $mails = Mail::find(array(
-            'conditions' => 'status = ?1 AND statisticsStatus = ?2 AND finishedon < ?3 AND finishedon > ?4',
+            'conditions' => 'status = ?1 AND statisticsStatus = ?2 AND finishedon <= ?3',
             'bind' => array(1 => 'Sent',
                             2 => 0,
-                            3 => $d,
-                            4 => $towdays)
+                            3 => $towdays,)
         ));        
         
         $dates = array();
